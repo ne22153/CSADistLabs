56d774d8fbe7d425c3397103c67415db7be447fa
unknown <ne22153@bristol.ac.uk>
2023-10-13 15:38:39 +0100

    Added md file for AMI setup

--- Lab 6/distributed-lab-2-master/secretstrings/client/client.go
+++ Lab 6/distributed-lab-2-master/secretstrings/client/client.go
@@ -1,16 +1,35 @@
 package main
+
 import (
-//	"net/rpc"
+	"bufio"
 	"flag"
-//	"bufio"
-//	"os"
-//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
 	"fmt"
+	"net/rpc"
+	"os"
+	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
 )
 
-func main(){
-	server := flag.String("server","127.0.0.1:8030","IP:port string to connect to as server")
+func main() {
+	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
 	flag.Parse()
 	fmt.Println("Server: ", *server)
-	//TODO: connect to the RPC server and send the request(s)
+	client, _ := rpc.Dial("tcp", *server)
+	defer client.Close()
+
+	file, err := os.Open("wordlist")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		request := stubs.Request{Message: scanner.Text()}
+		response := new(stubs.Response)
+		client.Call(stubs.PremiumReverseHandler, request, response)
+		fmt.Println("Responded: ", response.Message)
+	}
+
 }
